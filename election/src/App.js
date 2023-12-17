254a5758b7d0fa718cf66c5edb956e71bb82e842
dhiraj19999 <yogeshsamse682@gmail.com>
2023-12-17 10:05:47 +0530

    add

---
+++ election/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Component/Navbar';
+import Project from './Pages/Project';
+//
+function App() {
+  return (
+    <div className="App">
+    
+    <Navbar/>
+    {/*  het */}
+    </div>
+  );
+}
+
+export default App;
